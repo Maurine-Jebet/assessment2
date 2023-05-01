7ffc69e5cc7fc5c9449f11aca418448217ac539e
Maurine-Jebet <maurinejebett@gmai.com>
2023-05-01 09:04:19 +0300

    first commit

---
+++ objects.js
@@ -0,0 +1,87 @@
+//1. Create a function called getAvailableBooks that returns an array of all available
+//books.
+
+function getAvailableBooks(){
+  
+   let books = ["encyclopaedia","jesma","bible","novel"  ]
+        
+        return getAvailableBooks;
+       
+ }
+ console.log()
+
+
+
+
+
+//2. Create a function getBooksByAuthor that takes an author's name as an argument and
+//returns an array of all books by that author.
+
+ function getBooksByAuthor(name){
+    novels = [];
+    let
+
+
+
+ }
+
+
+
+//3. Create a function addNewBook that takes a book object as an argument and adds it
+//to the library, ensuring that the new book has all required properties (title, author,
+//publicationYear, and isAvailable).
+
+function addNewBook(book){
+     let previousBook = [book = "title","author","publicationYear,isAvailable"]
+        book = ["encyclopaedia","jesma","bible","novel" ]
+        (story.push(book))
+        console.log(previousBook)
+
+        }
+        
+        addNewBook(book)
+     
+
+
+
+
+
+
+
+//4. Create a function checkoutBook that takes a book title as an argument and changes
+//the book's isAvailable property to false. If the book is not found in the library, the
+//function should return a message indicating that the book is not available.
+
+
+function checkoutBook(title){
+    book = ["encyclopaedia","jesma","bible","novel" ]
+    if(bible == bible)
+    return "true"
+    else{
+
+        (bible =! bible)
+        return "false"
+
+    }
+        
+    }
+        
+
+    
+
+
+
+
+
+
+
+
+//5. Create a function returnBook that takes a book title as an argument and changes the
+//book's isAvailable property to true. If the book is not found in the library, the function
+//should return a message indicating that the book does not belong to the library.
+
+
+function returnBook(bookTitle){
+
+
+}
